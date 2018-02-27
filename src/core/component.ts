997994e33bed9ce8caf3f717e6112e39864b5c58
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-02-27 16:50:16 +0300

    Remove css imports away from component

--- src/core/component.ts
+++ src/core/component.ts
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import "devextreme/dist/css/dx.common.css";
-import "devextreme/dist/css/dx.light.compact.css";
-
 export default class Component<P> extends React.Component<P, any> {
 
   protected WidgetClass: any;
