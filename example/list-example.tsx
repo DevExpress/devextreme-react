83af14fed68791c2126e9a6dd382585879f6ef77
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-04-20 13:40:43 +0300

    Fix build

--- example/list-example.tsx
+++ example/list-example.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import DataSource from "devextreme/data/data_source";
-import { Template } from "../src/core/template";
 
 import { Button } from "../src/ui/button";
 import { List } from "../src/ui/list";
@@ -83,13 +82,7 @@ export default class extends React.Component<any, { text: string; items: IListIt
                     items={this.state.items}
                     itemComponent={Item}
                 />
-                <h4>List with standalone template</h4>
-                <List
-                    items={this.state.items}
-                >
-                    <Template name={"item"} component={Item}/>
-                </List>
-
+                
                 <hr />
                 <h4>List with dataSource</h4>
                 <List dataSource={this.dataSource} />
