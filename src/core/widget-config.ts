b361fa5c0b370d4f5c20aef7a6c651013d242dd5
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2018-09-21 14:41:09 +0300

    Fix className forwarding (#192)

--- src/core/widget-config.ts
+++ src/core/widget-config.ts
@@ -1,20 +1,22 @@
 import { ITemplateMeta } from "./template";
 
-const elementPropNames = ["style", "className", "id"];
+const elementPropNames = ["style", "id"];
 
 function separateProps(
     props: Record<string, any>,
     defaultsProps: Record<string, string>,
-    templateProps: ITemplateMeta[]
+    templateProps: ITemplateMeta[],
 ): {
     options: Record<string, any>;
     defaults: Record<string, any>;
     templates: Record<string, any>;
+    classNames: string[] | null;
 } {
     templateProps = templateProps || [];
     const defaults: Record<string, any> = {};
     const options: Record<string, any> = {};
     const templates: Record<string, any> = {};
+    let classNames: string[] | null = null;
 
     const knownTemplates: Record<string, any> = {};
 
@@ -27,23 +29,31 @@ function separateProps(
         const defaultOptionName = defaultsProps ? defaultsProps[key] : null;
 
         if (isIgnoredProp(key)) {
+            if (key === "className") {
+                classNames = props[key].split(" ");
+            }
+
             return;
         }
 
         if (defaultOptionName) {
             defaults[defaultOptionName] = props[key];
-        } else if (knownTemplates[key]) {
-            templates[key] = props[key];
-        } else {
-            options[key] = props[key];
+            return;
         }
+
+        if (knownTemplates[key]) {
+            templates[key] = props[key];
+            return;
+        }
+
+        options[key] = props[key];
     });
 
-    return { options, defaults, templates };
+    return { options, defaults, templates, classNames };
 }
 
-function isIgnoredProp(name) {
-    return name === "children" || elementPropNames.indexOf(name) > 0;
+function isIgnoredProp(name: string) {
+    return name === "children" || name === "className" || elementPropNames.indexOf(name) > -1;
 }
 
 export {
