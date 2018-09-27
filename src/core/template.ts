7ee2bf74c3f48ee30d7f9f3de2a747bc5c7d734b
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-09-27 19:12:37 +0300

    Add semicolons

--- src/core/template.ts
+++ src/core/template.ts
@@ -21,10 +21,10 @@ class Template extends React.PureComponent<ITemplateProps, any> {
 
 const requiredPropsCheck = (props: Record<string, any>) => {
     if (!props.component && !props.render) {
-        return new Error("One of 'component' or 'render' is required by Template component.")
+        return new Error("One of 'component' or 'render' is required by Template component.");
     }
     return null;
-}
+};
 
 (Template as any).propTypes = {
     name: PropTypes.string.isRequired,
