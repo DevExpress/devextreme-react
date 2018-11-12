71f1f4573dfcbc4dd788c3680fb588bbbeb591a1
Ilya <ilyakhd@users.noreply.github.com>
2018-11-12 22:35:38 +0300

    Update map example

--- example/map-example.tsx
+++ example/map-example.tsx
@@ -3,11 +3,33 @@ import Example from "./example-block";
 
 import Map, { Location, Marker, Route, Tooltip } from "../src/map";
 
-export default class extends React.Component<any, { text: string; uncontrolledText: string; }> {
+interface IPosition {
+    lat: number;
+    lng: number;
+}
+
+const startPos = {
+    lat: 40.71000,
+    lng: -73.91000
+};
+
+export default class extends React.Component<any, { text: string; pos: IPosition }> {
+
+    constructor(props: any) {
+        super(props);
+        const pos = { ...startPos };
+        this.state = {
+            text: JSON.stringify(pos),
+            pos
+        };
+
+        this.updatePos = this.updatePos.bind(this);
+    }
 
     public render() {
         return (
-            <Example title="dxMap">
+            <Example title="dxMap" state={this.state}>
+                <button onClick={this.updatePos}>Move!</button>
                 <Map
                     provider={"bing"}
                     zoom={11}
@@ -24,12 +46,36 @@ export default class extends React.Component<any, { text: string; uncontrolledTe
                         <Location lat={40.7825} lng={-73.966111} />
                     </Marker>
 
+                    <Marker iconSrc="https://js.devexpress.com/Demos/RealtorApp/images/map-marker.png">
+                        <Tooltip text={"Start"} />
+                        <Location {...startPos} />
+                    </Marker>
+
+                    <Marker>
+                        <Tooltip text={JSON.stringify(this.state.pos)} />
+                        <Location {...this.state.pos} />
+                    </Marker>
+
                     <Route color="green">
                         <Location lat={40.755833} lng={-73.986389} />
                         <Location lat={40.7825} lng={-73.966111} />
+                        <Location {...startPos} />
+                        <Location {...this.state.pos} />
                     </Route>
                 </Map>
             </Example>
         );
+    }
+
+    public updatePos() {
+        const pos = {
+            lat: this.state.pos.lat + 0.01,
+            lng: this.state.pos.lng + 0.05
+        };
+
+        this.setState({
+            text: JSON.stringify(pos),
+            pos
+        });
     }
 }
