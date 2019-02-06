fc7d6d030ccbfb291ce61ec4f3d85b09e44bc0a4
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2019-02-06 13:59:00 +0300

    Fix inline templates pulling (#300)

--- src/core/options-manager.ts
+++ src/core/options-manager.ts
@@ -1,6 +1,7 @@
 import { ITemplateMeta } from "./template";
 
-import { addPrefixToKeys, getNestedValue, isEmptyObject } from "./helpers";
+import { Children as ReactChildren } from "react";
+import { addPrefixToKeys, getNestedValue } from "./helpers";
 import { createOptionComponent, INestedOptionMeta } from "./nested-option";
 import TemplateHost from "./template-host";
 import { separateProps } from "./widget-config";
@@ -217,10 +218,13 @@ class OptionsManager {
 
             const nestedObjects = this._getNestedOptionsObjects(e.children, templateRegistrationRequired);
 
+            const hasChildrenForTemplate =
+                ReactChildren.count(e.element.props.children) > Object.keys(nestedObjects).length;
+
             if (templateRegistrationRequired) {
                 this._templateHost.add({
                     useChildren: (optionName) => {
-                        return optionName === "template" && !!e.element.props.children && isEmptyObject(nestedObjects);
+                        return optionName === "template" && hasChildrenForTemplate;
                     },
                     props: props.templates,
                     templateProps: configComponent.templates,
