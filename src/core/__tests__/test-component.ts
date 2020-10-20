819a755de0f520a940683ec2f6d70a70fda9710e
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2020-10-20 13:20:33 +0300

    Remove unnecessary changes

--- src/core/__tests__/test-component.ts
+++ src/core/__tests__/test-component.ts
@@ -1,5 +1,4 @@
 import { Component } from '../../core/component';
-import { capitalizeFirstLetter } from '../helpers';
 
 const eventHandlers: { [index: string]: (e?: any) => void } = {};
 
@@ -18,13 +17,6 @@ const WidgetClass = jest.fn(() => Widget);
 
 class TestComponent<P = any> extends Component<P> {
   protected _WidgetClass = WidgetClass;
-
-  protected _defaults = Object.keys(this.props).reduce((acc, p) => {
-    if (!p.startsWith('on')) {
-      acc[`default${capitalizeFirstLetter(p)}`] = p;
-    }
-    return acc;
-  }, {});
 }
 
 function fireOptionChange(fullName: string, value: any) {
