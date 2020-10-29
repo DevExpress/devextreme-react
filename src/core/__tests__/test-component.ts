ba3009a191ec38296166843467033665d78f0c42
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2020-10-29 15:35:21 +0300

    Fix lint

--- src/core/__tests__/test-component.ts
+++ src/core/__tests__/test-component.ts
@@ -18,7 +18,7 @@ const WidgetClass = jest.fn(() => Widget);
 class TestComponent<P = any> extends Component<P> {
   protected _WidgetClass = WidgetClass;
 
-  _createWidget(element?: Element) {
+  _createWidget(element?: Element): void {
     Widget.option.mockImplementation((name: string) => name === 'integrationOptions.useDeferUpdateForTemplates');
 
     super._createWidget(element);
