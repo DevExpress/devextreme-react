e58ff95c702ac4661e3a35c4e791539cdc8d5038
Elizaveta Galuza (DevExpress) <galuza.elizaveta@corp.devexpress.com>
2021-02-17 16:31:25 +0300

    Formatting

--- src/core/configuration/__tests__/tree.test.ts
+++ src/core/configuration/__tests__/tree.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-classes-per-file */
-import { findValueInObject } from '../tree';
+import { findValueInObject } from "../tree";
 
-describe('check findValueInObject', () => {
+describe("check findValueInObject", () => {
 
     it("get underfined by string", () => {
         const obj = "Extension";
@@ -13,19 +13,19 @@ describe('check findValueInObject', () => {
 
     it("get value by object", () => {
         const obj = {
-            "sortOrder": "desc"
-        }
+            sortOrder: "desc"
+        };
         const path = ["sortOrder"];
         const valueInObject = findValueInObject(obj, path);
-        expect(valueInObject).toEqual({ "type": 0, "value": "desc" });
+        expect(valueInObject).toEqual({ type: 0, value: "desc" });
         expect(() => findValueInObject(obj, path)).not.toThrow();
-    })
+    });
 
-    it("get underfined by empty object",() =>{
-        const obj = {}
+    it("get underfined by empty object", () => {
+        const obj = {};
         const path = ["sortOrder"];
         const valueInObject = findValueInObject(obj, path);
         expect(valueInObject).toEqual(undefined);
         expect(() => findValueInObject(obj, path)).not.toThrow();
-    })
-})
+    });
+});
