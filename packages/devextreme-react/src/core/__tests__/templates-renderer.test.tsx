c141e419fa2ca244c2e3de1c2016d6435b60a2dd
Elizaveta Galuza (DevExpress) <galuza.elizaveta@corp.devexpress.com>
2021-03-11 10:30:23 +0300

    Add test for number of function calls

--- packages/devextreme-react/src/core/__tests__/templates-renderer.test.tsx
+++ packages/devextreme-react/src/core/__tests__/templates-renderer.test.tsx
@@ -7,81 +7,80 @@ import { TemplatesRenderer } from '../templates-renderer';
 import { TemplatesStore } from '../templates-store';
 
 global.console.warn = (message) => {
-    throw message;
+  throw message;
 };
 
 global.console.error = (message) => {
-    throw message;
+  throw message;
 };
 
 jest.mock('devextreme/animation/frame', () => ({
-    requestAnimationFrame: jest.fn(),
+  requestAnimationFrame: jest.fn(),
 }));
 
 jest.mock('devextreme/core/utils/common', () => ({
-    deferUpdate: jest.fn(),
+  deferUpdate: jest.fn(),
 }));
 
 describe('useDeferUpdate ==$(value)', () => {
-    let updateFunc;
+  let updateFunc;
 
-    [true, false].forEach((value) => {
+  [true, false].forEach((value) => {
+    beforeEach(() => {
+      (deferUpdate as jest.Mock).mockImplementation((func) => {
+        updateFunc = func;
+      });
 
-        beforeEach(() => {
-            (deferUpdate as jest.Mock).mockImplementation((func) => {
-                updateFunc = func;
-            });
-
-            (requestAnimationFrame as jest.Mock).mockImplementation((func) => {
-                updateFunc = func;
-            });
-        });
-
-        afterEach(() => {
-            jest.clearAllMocks();
-        });
-
-        it('should not throw warning when unmounted', async () => {
-            const ref = React.createRef<TemplatesRenderer>();
-            const templatesStore = new TemplatesStore(() => { });
-
-            const component = render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
-
-            expect(ref.current).not.toBeNull();
-            expect(() => ref.current?.scheduleUpdate(value)).not.toThrow();
-
-            expect(() => updateFunc()).not.toThrow();
-
-            component.unmount();
-            expect(ref.current).toBeNull();
-
-            expect(() => updateFunc()).not.toThrow();
-        });
-
-        it('should work deferUpdate && requestAnimationFrame', async () => {
-            const ref = React.createRef<TemplatesRenderer>();
-            const templatesStore = new TemplatesStore(() => { });
-
-            render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
-            expect(() => ref.current?.scheduleUpdate(value)).not.toThrow();
-
-            if (value) {
-                expect(deferUpdate).toHaveBeenCalledTimes(1);
-            } else {
-                expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
-            }
-        });
-
-        it('should not call twice', async () => {
-            const ref = React.createRef<TemplatesRenderer>();
-            const templatesStore = new TemplatesStore(() => { });
-
-            render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
-            expect(() => ref.current?.scheduleUpdate(value)).not.toThrow();
-            
-            expect(() => { ref.current?.scheduleUpdate(value);
-
-            }).toHaveBeenCalledTimes(1);
-        })
+      (requestAnimationFrame as jest.Mock).mockImplementation((func) => {
+        updateFunc = func;
+      });
     });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should not throw warning when unmounted', async () => {
+      const ref = React.createRef<TemplatesRenderer>();
+      const templatesStore = new TemplatesStore(() => { });
+
+      const component = render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
+
+      expect(ref.current).not.toBeNull();
+      expect(() => ref.current?.scheduleUpdate(value)).not.toThrow();
+
+      expect(() => updateFunc()).not.toThrow();
+
+      component.unmount();
+      expect(ref.current).toBeNull();
+
+      expect(() => updateFunc()).not.toThrow();
+    });
+
+    it('should work deferUpdate && requestAnimationFrame', async () => {
+      const ref = React.createRef<TemplatesRenderer>();
+      const templatesStore = new TemplatesStore(() => { });
+
+      render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
+      expect(() => ref.current?.scheduleUpdate(value)).not.toThrow();
+
+      if (value) {
+        expect(deferUpdate).toHaveBeenCalledTimes(1);
+      } else {
+        expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
+      }
+    });
+
+    it('should not call twice', async () => {
+      const ref = React.createRef<TemplatesRenderer>();
+      const templatesStore = new TemplatesStore(() => { });
+
+      render(<TemplatesRenderer templatesStore={templatesStore} ref={ref} />);
+      ref.current?.scheduleUpdate(value);
+      expect(updateFunc).toHaveBeenCalledTimes(1);
+
+      ref.current?.scheduleUpdate(value);
+      expect(updateFunc).toHaveBeenCalledTimes(0);
+    });
+  });
 });
