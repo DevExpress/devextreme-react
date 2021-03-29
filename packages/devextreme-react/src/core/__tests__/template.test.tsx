728096c26efbc2704f47c5122c665e706cf6d61a
SelyankinFyodor <fedkahuligan@gmail.com>
2021-03-29 10:27:15 +0300

    - add test-case for onOptionChanged - eslint fixes

--- packages/devextreme-react/src/core/__tests__/template.test.tsx
+++ packages/devextreme-react/src/core/__tests__/template.test.tsx
@@ -172,8 +172,8 @@ function testTemplateOption(testedOption: string) {
     const container = document.createElement('div');
     renderItemTemplate({ text: 'with data' }, container);
     component.update();
-    expect(container.innerHTML).
-      toBe('<div style=\"display: none;\"></div>Template with data<span style=\"display: none;\"></span>');
+    expect(container.innerHTML)
+      .toBe('<div style=\"display: none;\"></div>Template with data<span style=\"display: none;\"></span>');
   });
 
   it('does not render template removeEvent listener', () => {
