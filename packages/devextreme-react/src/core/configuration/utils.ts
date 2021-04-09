ee33ee9dcb137f6a19a861b7b7c0bea780610890
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2021-04-09 12:56:01 +0300

    Refactor

--- packages/devextreme-react/src/core/configuration/utils.ts
+++ packages/devextreme-react/src/core/configuration/utils.ts
@@ -30,9 +30,9 @@ interface ICollectionOptionInfo {
   index: number;
 }
 
-export const isIE = () => {
+export const isIE = (): boolean => {
   const ua = window?.navigator?.userAgent ?? ''; // Check the userAgent property of the window.navigator object
-  const msie = ua.indexOf('MSIE '); // IE 10 or older
+  const msie = ua.indexOf('MSIE'); // IE 10 or older
   const trident = ua.indexOf('Trident/'); // IE 11
 
   return (msie > 0 || trident > 0);
