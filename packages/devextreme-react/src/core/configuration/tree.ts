cd0c7514f5d5f139079a0d7031f92d202b3209cc
SelyankinFyodor <fedkahuligan@gmail.com>
2021-05-27 11:02:15 +0300

    - fix Rollback complex options by fields (T988609) (#537)  - eslint fix  - add tests

--- packages/devextreme-react/src/core/configuration/tree.ts
+++ packages/devextreme-react/src/core/configuration/tree.ts
@@ -86,7 +86,8 @@ function findValueInObject(obj: unknown, path: string[]): undefined | IValueDesc
   if (!key) {
     return {
       value: obj,
-      type: obj instanceof Object ? ValueType.Complex : ValueType.Simple,
+      type: obj instanceof Object && !(obj instanceof Array) ? ValueType.Complex : ValueType.Simple,
+      // type: obj instanceof Object ? ValueType.Complex : ValueType.Simple,
     };
   }
 
