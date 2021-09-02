bb29d7835bc463d923908a50ba86201516bc9f6f
egaluza <lizagaluza@mail.ru>
2021-09-02 18:11:40 +0300

    Refactor

--- packages/devextreme-react-generator/src/generator.ts
+++ packages/devextreme-react-generator/src/generator.ts
@@ -157,7 +157,7 @@ export function collectIndependentEvents(options: IProp[]): IProp[] {
     if (option.types.filter((type) => type.type === 'Function').length === 1
         && (!option.firedEvents || option.firedEvents.length === 0)
         && option.name.substr(0, 2) === 'on'
-        && !option.name.match(/^(?!.*alue).*Changed/)
+        && !option.name.match(/^(?!.*Value).*Changed/)
     ) {
       acc.push(option);
     }
