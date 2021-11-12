fba3ac021c2d092359e1fcbbfd46231f962aeabd
Alexey Serov <alexserov@users.noreply.github.com>
2021-11-12 17:21:11 +0300

    Update versions to 22.1 (#631)

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const eslint = require("gulp-eslint");
 
 gulp.task('lint', () => gulp.src([
   'packages/sandbox/*.ts{,x}', 
-  'packages/devextreme-*/src/**/*.ts{,x}'
+  'packages/devextreme-*/src/**/*.ts{,x}',
+  '!**/node_modules/**/*'
 ])
   .pipe(eslint({ configFile: '.eslintrc', fix: true }))
   .pipe(eslint.format())
