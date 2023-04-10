f86852d8040696abbe65d30f4d18d714f00d2bf4
alexlavrov <alex.s.lavrov@gmail.com>
2023-04-10 17:08:51 +0400

    Fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp');
-const eslint = require("gulp-eslint");
+const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => gulp.src([
-  'packages/sandbox/*.ts{,x}', 
+  'packages/sandbox/*.ts{,x}',
   'packages/devextreme-*/src/**/*.ts{,x}',
-  '!**/node_modules/**/*'
+  '!**/node_modules/**/*',
 ])
   .pipe(eslint({ configFile: '.eslintrc', fix: true }))
   .pipe(eslint.format())
